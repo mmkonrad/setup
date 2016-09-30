61097612164b7a606b0ba3c0656e6717fdba4dc9
mmkonrad <M.M.Konrad@web.de>
2016-09-30 11:04:49 +0200

    init setup content

---
+++ swarm_node.sh
@@ -0,0 +1,9 @@
+sudo docker stop $(sudo docker ps -aq)
+sudo docker rm -v $(sudo docker ps -aq)
+
+ADVERTISE_ADDRESS=`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
+
+sudo docker run -d --net=reservierung swarm join --advertise $ADVERTISE_ADDRESS:2375 consul://$1:8500
+
+sudo docker pull bachelorproject/msi
+
